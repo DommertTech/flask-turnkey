39ccce059cf9cab2f35be4ccb81134cbfc25d370
Dommert <DommertEnterprises@Gmail.com>
2014-11-21 22:50:28 -0700

    rum config changes

--- flask_starter/rum.py
+++ flask_starter/rum.py
@@ -3,6 +3,7 @@ from flask_rum.main import rum
 #import flask_rum.rum_config as rum_config
 #app.config.from_object(rum_config)
 app.config.THEME_FOLDER='rum/banana/'
+app.config.PROJECT_TITLE = '[Flask-Rum]'
 app.config.TEMPLATE_DEFAULTS = {
 'nav': 'site/blocks/rum_nav.html',
 'footer': 'site/blocks/rum_footer.html'
