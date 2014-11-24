dba9b4dc88a39eb6d456d77d6f4c8497a30f92fd
Dommert <DommertEnterprises@Gmail.com>
2014-11-24 13:58:24 -0700

    refactored to flask_turnkey

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='flask-turnkey',
     version='0.0.1',
-    packages=['flask_starter.flask_rum', 'flask_starter.flask_turboduck', 'flask_starter.flask_turboduck.tests'],
+    packages=['flask_turnkey.flask_rum', 'flask_turnkey.flask_turboduck', 'flask_turnkey.flask_turboduck.tests'],
     url='http://github.com/dommerttech/flask-turnkey',
     license='mit',
     author='Dommert',
